34e6a80f07f616c0db81f61789cd9c9b902c4b62
Bruno Cuevas <brunocue92@gmail.com>
2023-10-11 13:24:56 +0200

    Cambio

--- practice_list.py
+++ practice_list.py
@@ -1,55 +1,34 @@
-def sumar_numeros_impares(numbers):
-    """Sum the odd numbers in a list of numbers and return the result."""
-    total = 0
-    for num in numbers:
-        if num % 2 != 0:
-            total += num
-    return total
+numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-def lista_unicos_ordenados(numeros):
-    """Devuelve una lista de elementos únicos de una lista, en el orden de la primera lista."""
-    unicos = []
-    for num in numeros:
-        if num not in unicos:
-            unicos.append(num)
-    return unicos
+while True:
+    print("MENU:")
+    print("1. Imprimir los primeros cinco elementos")
+    print("2. Imprimir números pares")
+    print("3. Imprimir números impares")
+    print("4. Imprimir múltiplos de 5")
+    print("5. Salir")
+    
+    opcion = input("Elija una opción: ")
 
-def numeros_pares(numeros):
-    """Devuelve una lista de números pares de una lista de números."""
-    pares = []
-    for num in numeros:
-        if num % 2 == 0:
-            pares.append(num)
-    return pares
-
-def numeros_impares(numeros):
-    """Devuelve una lista de números impares de una lista de números."""
-    impares = []
-    for num in numeros:
-        if num % 2 != 0:
-            impares.append(num)
-    return impares
-
-
-print ("\nEJERCICIO SUMAR NUMEROS IMPARES")
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-resultado = sumar_numeros_impares(numeros)
-print("La suma de números impares es:", resultado)
-
-print ("\nEJERCICIO NUMEROS PARES")
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-pares = numeros_pares(numeros)
-print("Números pares:", pares)
-
-
-print ("\nEJERCICIO NUMEROS IMPARES")
-numeros = [1, 2, 3, 4, 5, 6, 7]
-unicos = numeros_impares(numeros)
-print("Elementos únicos:", unicos)
-
-
-print ("\nEJERCICIO LISTA UNICOS(NUMEROS NO REPETIDOS)")
-numeros = [1, 6, 2, 3, 4, 4, 5, 5,2,6]
-unicos = lista_unicos_ordenados(numeros)
-print("Elementos únicos:", unicos)
-
+    if opcion == "1":
+        print(numList[:5])
+    elif opcion == "2":
+        print("Números pares:")
+        for num in numList:
+            if num % 2 == 0:
+                print(num)
+    elif opcion == "3":
+        print("Números impares:")
+        for num in numList:
+            if num % 2 != 0:
+                print(num)
+    elif opcion == "4":
+        print("Múltiplos de 5:")
+        for num in numList:
+            if num % 5 == 0:
+                print(num)
+    elif opcion == "5":
+        print("¡Hasta luego!")
+        break
+    else:
+        print("Opción no válida. Por favor, elija una opción del 1 al 5.")
